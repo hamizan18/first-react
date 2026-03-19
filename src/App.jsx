a9d8a2bdb0a511510bdf5cad9b31290a4dcc36c1
hamizan18 <hamizanputra321@gmail.com>
2026-03-19 15:45:40 +0700

    lev 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,17 @@
-import { useState, useEffect } from 'react'
+import {useState} from 'react';
 
 function App() {
-  const [todos, setTodos] = useState(() => {
-    const saved = localStorage.getItem("todos");
-    return saved ? JSON.parse(saved) : [];
-  });
-  const [input, setInput] = useState("");
+  const notes = [
+    {id: 1, title: "belajar react"},
+    {id: 2, title: "tanpa supabase"}
+  ];
 
-  const addTodo = () => {
-    if (input.trim() === "") return;
-
-    setTodos([
-      ...todos,
-      { id: Date.now(), text: input }
-    ]);
-
-    setInput("");
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-return (
-  <>
-    <h1>To-Do List</h1>
-
-  <input 
-    type='text'
-    value={input}
-    onChange={(e) => setInput(e.target.value)}
-    onKeyDown={(e) => e.key === "Enter" && addTodo()}
-  />
-  <button onClick={addTodo}>Tambah</button>
-
-  <ul>
-    {todos.map(todo => (
-      <li key={todo.id}>
-        {todo.text}
-        <button onClick={() => deleteTodo(todo.id)}>Hapuz</button>
-      </li>
-    ))}
-  </ul>
-
-  </>
+  return (
+    <>
+      {notes.map(note => (
+        <h1 key={note.id}>{note.title}</h1>
+      ))}
+    </>
   );
 }
 
